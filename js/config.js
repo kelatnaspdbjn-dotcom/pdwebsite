3bb0ca09dbf94001790295bd28234560655038c1
kelatnaspdbjn-dotcom <kelatnaspdbjn@gmail.com>
2026-07-31 10:57:52 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
 const CONFIG = {
-    SHEET_ID: '1hvMHE__krMFi5RevklY0JibAf9GXPKPCemQUb5EpnYQ',
-    API_KEY: 'AIzaSyDmQ9fNtNwK4u7X8Y9Z0A1B2C3D4E5F6G7H8I9J0',
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw__WnxW65wpxwWOmReR0DodrCFb8N5wgtQ5kr5mcAV5b0rXaNMwqEpj7o9R74f_6oNnQ/exec',
+    // Hapus SHEET_ID dan API_KEY (tidak perlu lagi)
+    // SHEET_ID: '...',
+    // API_KEY: '...',
+    
+    // Ganti dengan URL Apps Script Anda
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwRM3jUd01ZMKH9Neh9LFwaNB8apa5mRVzU0xnSi6BTibAuXGd50eM2-cbX2cIgljzaBA/exec',
     
     SHEETS: {
         BERITA: 'berita',
@@ -14,19 +17,10 @@ const CONFIG = {
         SURAT_KELUAR: 'surat_keluar',
         KEUANGAN: 'keuangan',
         CATATAN: 'catatan'
-    },
-    
-    GITHUB: {
-        REPO: 'username/perisai-diri',
-        BRANCH: 'main',
-        TOKEN: 'github_token_here'
     }
 };
 
+// Fungsi untuk mengambil data
 function getGoogleSheetsURL(sheetName) {
-    return `https://sheets.googleapis.com/v4/spreadsheets/${CONFIG.SHEET_ID}/values/${sheetName}?key=${CONFIG.API_KEY}`;
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { CONFIG, getGoogleSheetsURL };
+    return `${CONFIG.GOOGLE_SCRIPT_URL}?sheet=${sheetName}`;
 }
